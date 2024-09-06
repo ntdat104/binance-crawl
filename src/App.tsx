581a79da1678f8bf580189610818b42c496554e2
ntdat104 <ntd.k108cva@gmail.com>
2024-09-06 17:33:32 +0700

    add kchart

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useAppDispatch } from "@/redux/hook";
 import React from "react";
-import AggSnap from "./components/AggSnap";
+// import AggSnap from "./components/AggSnap";
 import { connect } from "./redux/slice/websocket-slice";
+import KlineChart from "./components/KlineChart";
 
 const App: React.FC = (): JSX.Element => {
   const dispatch = useAppDispatch();
@@ -15,8 +16,9 @@ const App: React.FC = (): JSX.Element => {
       <div className="flex">
         {/* <OrderBook className="!w-80 min-w-80 h-screen" /> */}
         {/* <Chart /> */}
-        <AggSnap />
+        {/* <AggSnap /> */}
       </div>
+      <KlineChart />
     </main>
   );
 };
