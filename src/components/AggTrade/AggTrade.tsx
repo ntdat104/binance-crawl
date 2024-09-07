f2a76aad065971b5fc200ed35768f7875866c9f1
ntdat104 <ntd.k108cva@gmail.com>
2024-09-07 23:35:53 +0700

    add order-form

--- src/components/AggTrade/AggTrade.tsx
+++ src/components/AggTrade/AggTrade.tsx
@@ -1,3 +1,4 @@
+import mainAxios from "@/api/main-axios";
 import { useAppSelector } from "@/redux/hook";
 import { formatPrice } from "@/utils/format-price";
 import clsx from "clsx";
@@ -6,68 +7,93 @@ import List from "rc-virtual-list";
 import React from "react";
 
 type AggTrades = {
+  M: boolean;
+  T: number;
+  a: number;
+  f: number;
+  l: number;
+  m: boolean;
+  p: string;
+  q: string;
+  E: number;
   e: string;
   s: string;
-  p: string;
-  cq: string;
-  T: number;
 };
 
 interface Props extends React.ComponentPropsWithoutRef<"div"> {}
 
-const AggSnap: React.FC<Props> = (props): JSX.Element => {
+const AggTrade: React.FC<Props> = (props): JSX.Element => {
   const { className, ...rest } = props;
-  const wsf = useAppSelector((state) => state.websocket.wsf);
+  const ws = useAppSelector((state) => state.websocket.ws);
   const symbol = useAppSelector((state) => state.websocket.symbol);
-  const [aggSnap, setAggSnap] = React.useState<AggTrades[]>([]);
+  const [aggTrades, setAggTrades] = React.useState<AggTrades[]>([]);
 
-  const aggSnapRef = React.useRef<HTMLDivElement>(null);
+  const aggTradeRef = React.useRef<HTMLDivElement>(null);
+
+  const limit = React.useMemo<number>(() => 80, []);
 
   React.useEffect(() => {
-    const subscriber = wsf.addSubscriber("AggSnap");
+    (async () => {
+      try {
+        const data: AggTrades[] = await mainAxios.get(
+          `/api/v1/aggTrades?limit=${limit}&symbol=${symbol}`
+        );
+        setAggTrades(data.reverse());
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [symbol, limit]);
+
+  React.useEffect(() => {
+    const subscriber = ws.addSubscriber("AggTrade");
 
     subscriber.send(
       JSON.stringify({
         method: "SUBSCRIBE",
-        params: [`${symbol.toLowerCase()}@aggSnap`],
+        params: [`${symbol.toLowerCase()}@aggTrade`],
         id: 1,
       })
     );
 
     subscriber.subscribe((event: MessageEvent<any>) => {
       const message = JSON.parse(event.data);
-      if (message?.stream === `${symbol.toLowerCase()}@aggSnap`) {
+      if (message?.stream === `${symbol.toLowerCase()}@aggTrade`) {
         const data = message?.data;
-        setAggSnap((aggSnap) => [data, ...aggSnap]);
+        setAggTrades((aggSnap) => {
+          const newAggTrades = [...aggSnap];
+          newAggTrades.pop();
+          newAggTrades.unshift(data);
+          return newAggTrades;
+        });
       }
     });
 
     return () => {
-      setAggSnap([]);
       subscriber.unsubscribe();
     };
   }, [symbol]);
 
   return (
     <div
-      ref={aggSnapRef}
+      ref={aggTradeRef}
       className={clsx("border border-solid border-[#2B3139]", className)}
       {...rest}
     >
       <div className="px-4 h-[44px] flex items-center border-b border-solid border-[#2B3139]">
-        <span className="text-sm text-[#eaecef]">{`Giao dịch (Future)`}</span>
+        <span className="text-sm text-[#eaecef]">{`Giao dịch`}</span>
       </div>
-      <div className="w-[300px] pb-3">
+      <div className="w-full pb-3">
         <div className="flex items-center px-4 pt-[8px] pb-[4px]">
           <span className="text-xs text-[#848e9c] w-[30%] text-left">{`Giá(USDT)`}</span>
           <span className="text-xs text-[#848e9c] w-[40%] text-right">{`Số lượng(BTC)`}</span>
           <span className="text-xs text-[#848e9c] w-[30%] text-right">{`Thời gian`}</span>
         </div>
         <List
-          data={aggSnap}
+          data={aggTrades}
           height={
-            aggSnapRef.current?.clientHeight
-              ? aggSnapRef.current?.clientHeight - 90
+            aggTradeRef.current?.clientHeight
+              ? aggTradeRef.current?.clientHeight - 90
               : 200
           }
           itemHeight={30}
@@ -85,12 +111,12 @@ const AggSnap: React.FC<Props> = (props): JSX.Element => {
             <div className="flex items-center h-[18px] px-4">
               <span
                 className="text-xs w-[30%] text-left"
-                style={{ color: Number(item?.cq) > 0 ? "#2ebd85" : "#f6465d" }}
+                style={{ color: !item?.m ? "#2ebd85" : "#f6465d" }}
               >
                 {formatPrice(item?.p)}
               </span>
               <span className="text-xs text-[#eaecef] w-[40%] text-right">
-                {formatPrice(Math.abs(Number(item?.cq)), 3)}
+                {formatPrice(Math.abs(Number(item?.q)), 5)}
               </span>
               <span className="text-xs text-[#eaecef] w-[30%] text-right">
                 {dayjs(item?.T).format("HH:mm:ss")}
@@ -103,4 +129,4 @@ const AggSnap: React.FC<Props> = (props): JSX.Element => {
   );
 };
 
-export default AggSnap;
+export default AggTrade;
